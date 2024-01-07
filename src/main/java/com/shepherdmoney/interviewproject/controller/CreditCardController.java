49da79124275c11834ca8fb550c7cfd116bc1938
sachinsr1 <114034505+sachinsr1@users.noreply.github.com>
2024-01-07 03:42:40 -0500

    Update CreditCardController.java

--- src/main/java/com/shepherdmoney/interviewproject/controller/CreditCardController.java
+++ src/main/java/com/shepherdmoney/interviewproject/controller/CreditCardController.java
@@ -34,10 +34,6 @@ public class CreditCardController {
 
     @PostMapping("/credit-card")
     public ResponseEntity<Integer> addCreditCardToUser(@RequestBody AddCreditCardToUserPayload payload) {
-        // TODO: Create a credit card entity, and then associate that credit card with user with given userId
-        //       Return 200 OK with the credit card id if the user exists and credit card is successfully associated with the user
-        //       Return other appropriate response code for other exception cases
-        //       Do not worry about validating the card number, assume card number could be any arbitrary format and length
         Optional<User> realUser = userRepository.findById(payload.getUserId());
         if (realUser.isPresent()) {
             User user = realUser.get();
@@ -53,8 +49,6 @@ public class CreditCardController {
 
     @GetMapping("/credit-card:all")
     public ResponseEntity<List<CreditCardView>> getAllCardOfUser(@RequestParam int userId) {
-        // TODO: return a list of all credit card associated with the given userId, using CreditCardView class
-        //       if the user has no credit card, return empty list, never return null
         Optional<User> realUser = userRepository.findById(userId);
         if (realUser.isPresent()) {
             List<CreditCardView> creditCardViews = new ArrayList<>();
@@ -72,8 +66,6 @@ public class CreditCardController {
 
     @GetMapping("/credit-card:user-id")
     public ResponseEntity<Integer> getUserIdForCreditCard(@RequestParam String creditCardNumber) {
-        // TODO: Given a credit card number, efficiently find whether there is a user associated with the credit card
-        //       If so, return the user id in a 200 OK response. If no such user exists, return 400 Bad Request
         Optional<CreditCard> realCreditCard = creditCardRepository.findByNumber(creditCardNumber);
         if (realCreditCard.isPresent()) {
             CreditCard creditCard = realCreditCard.get();
@@ -88,9 +80,6 @@ public class CreditCardController {
 @PostMapping("/credit-card:update-balance")
 public ResponseEntity<String> updateBalanceHistory(@RequestBody UpdateBalancePayload[] payload) {
     //TODO: Given a list of transactions, update credit cards' balance history.
-        //      For example: if today is 4/12, a credit card's balanceHistory is [{date: 4/12, balance: 110}, {date: 4/10, balance: 100}],
-        //      Given a transaction of {date: 4/10, amount: 10}, the new balanceHistory is
-        //      [{date: 4/12, balance: 120}, {date: 4/11, balance: 110}, {date: 4/10, balance: 110}]
         //      Return 200 OK if update is done and successful, 400 Bad Request if the given card number
         //        is not associated with a card.    
     //loop over all transactions in the array
